ba487c3989854f3ef31c59efa1f926f16e75e376
Moritz Onken <onken@netcubed.de>
2015-06-14 13:54:24 +0200

    support bindings

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 (function(dependencies, definition) {
   if (typeof module === 'object' && module && module.exports) {
-      dependencies = dependencies.map(require);
-      module.exports = definition.apply(context, dependencies);
+    dependencies = dependencies.map(require);
+    module.exports = definition.apply(context, dependencies);
   } else if (typeof require === 'function') {
     define((dependencies || []), definition);
   }
@@ -11,6 +11,24 @@
     render: function() {
       Chaplin.View.prototype.render.apply(this, arguments);
       this.doLayout(this.config.toJSON());
+
+      _.keys(this.bindings || {}).forEach(function(selector) {
+        var attrs = this.bindings[selector];
+        var fn = _.isArray(attrs) ? attrs[attrs.length - 1] : _.identity;
+        attrs = _.isArray(attrs) ? attrs.slice(0, attrs.length - 1) : [attrs];
+
+        attrs.forEach(function(attr) {
+          var cb = function(model, value) {
+            this.$(selector).text(fn.apply(model, [value]));
+          }
+          cb.apply(this, [this.model, this.model.get(attr)]);
+          if (this.model.relationships[attr])
+            this.listenTo(this.model.get(attr), 'add remove reset', cb);
+          else
+            this.listenTo(this.model, 'change:' + attr, cb);
+        }, this);
+      }, this);
+
       return this;
     },
   });
