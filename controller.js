a0b459d1a5255edf7fe99603d761615075bab27e
Moritz Onken <onken@netcubed.de>
2017-03-09 17:14:53 -0800

    add documentaiton

--- controller.js
+++ controller.js
@@ -3,6 +3,16 @@ define([
   'underscore',
 ], function(Chaplin, _) {
   return Chaplin.Controller.extend({
+    /**
+     * Extends Chaplin's `listenTo` function to accept objects which simplifies definining multiple events.
+     *   this.listenTo(this.view, {
+     *     'model change': _.indentity(),
+     *   });
+     *
+     * @param {Component} component   Component that the listener will be attached to
+     * @param {Object} [[options]] 	Object with events and functions
+     * @return {Object} [[context]] Optional context
+     */
     listenTo: function(component, obj, context) {
       var listenTo = Chaplin.Controller.prototype.listenTo;
       if(_.isString(obj)) return listenTo.apply(this, arguments);
